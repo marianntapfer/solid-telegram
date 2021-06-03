113896432c4aa7722c540031aa04b0cb95d2bfe2
Automated Publisher <actions@users.noreply.github.com>
2021-06-03 07:10:36 +0000

    Automated publish: Thu Jun  3 07:10:36 UTC 2021 f1abaec6c4cd573caf1aac713dec1be2a43c409f

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -3,13 +3,38 @@
 // StyleDictionaryColor.h
 //
 // Do not edit directly
-// Generated on Thu, 03 Jun 2021 06:41:35 GMT
+// Generated on Thu, 03 Jun 2021 07:10:35 GMT
 //
 
 #import <UIKit/UIKit.h>
 
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+BordersUnsupportedMultipleBordersStroke,
+BordersSingleStroke,
+BordersSingleStyleStroke,
+BordersDashedOutsideStroke,
+ColorsMultipleFills0,
+ColorsMultipleFills1,
+ColorsSingleBlue,
+ColorsSpecialCharacters😅,
+ColorsSpecialCharactersAnderung,
+GradientMultiple0Stops0Color,
+GradientMultiple0Stops1Color,
+GradientMultiple1Stops0Color,
+GradientMultiple1Stops1Color,
+GradientMultiple2Stops0Color,
+GradientMultiple2Stops1Color,
+GradientMultiple3Stops0Color,
+GradientMultiple3Stops1Color,
+GradientSingleWithMultipleColorStopsStops0Color,
+GradientSingleWithMultipleColorStopsStops1Color,
+GradientSingleWithMultipleColorStopsStops2Color,
+GradientSingleWithMultipleColorStopsStops3Color,
+EffectDropShadowSingleColor,
+EffectInnerShadowMultiple0Color,
+EffectInnerShadowMultiple1Color,
+EffectInnerShadowMultiple2Color,
 ColorComponentButtonColorAlias,
 ColorGlobalNegative,
 ColorGlobalInfo,
--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Thu, 03 Jun 2021 06:41:35 GMT
+// Generated on Thu, 03 Jun 2021 07:10:35 GMT
 //
 
 #import "StyleDictionaryColor.h"
@@ -21,6 +21,31 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
+rgba(255, 230, 0, 1),
+rgba(112, 141, 242, 1),
+rgba(4, 74, 255, 1),
+rgba(64, 255, 186, 1),
+rgba(64, 255, 186, 1),
+rgba(0, 0, 0, 0.1),
+rgba(4, 74, 255, 1),
+rgba(64, 223, 80, 1),
+rgba(52, 86, 175, 1),
+rgba(255, 184, 0, 1),
+rgba(255, 184, 0, 1),
+rgba(255, 255, 255, 1),
+rgba(255, 255, 255, 1),
+rgba(207, 48, 48, 1),
+rgba(255, 255, 255, 1),
+rgba(74, 79, 204, 1),
+rgba(255, 255, 255, 1),
+rgba(255, 184, 0, 1),
+rgba(255, 138, 0, 1),
+rgba(255, 46, 0, 1),
+rgba(255, 0, 0, 1),
+rgba(0, 0, 0, 0.25),
+rgba(0, 0, 0, 0.25),
+rgba(0, 0, 0, 1),
+rgba(0, 0, 0, 0.25),
 [UIColor colorWithRed:0.839f green:0.259f blue:0.212f alpha:1.000f],
 [UIColor colorWithRed:0.839f green:0.259f blue:0.212f alpha:1.000f],
 [UIColor colorWithRed:0.184f green:0.431f blue:0.773f alpha:1.000f],
